8b6788293cd5c10baf2579b6aa7783398ad8b4d2
mumapada <65493584+mumapada@users.noreply.github.com>
2020-05-17 19:05:20 +0530

    Update SocketServer.java
    changes in classname

--- SocketServer.java
+++ SocketServer.java
@@ -6,7 +6,7 @@ import java.util.Timer;
  */
 
 //Main class
-public class SchedulerMain {
+public class SocketServer {
 	public static void main(String args[]) throws InterruptedException {
 
        try {
