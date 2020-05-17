922a7942e5b8e99f3d8322c4a9325e87411e2573
Umapada Mandal <umapada.mandal@gmail.com>
2020-05-17 18:57:34 +0530

    Update SocketServer.java

--- SocketServer.java
+++ SocketServer.java
@@ -1,1 +1,34 @@
-# First source for SocketServer
+import java.util.Timer;
+
+/**
+ *
+ * @author Umapada M.
+ */
+
+//Main class
+public class SchedulerMain {
+	public static void main(String args[]) throws InterruptedException {
+
+       try {
+		Timer time = new Timer(); // Instantiate Timer Object
+		SocketServer server = new SocketServer(8899); // Instantiate SheduledTask class
+		time.schedule(server, 0, 120000); // Create Repetitively task for every 1 secs
+		//time.schedule(server, 0, 43200000); // Create Repetitively task for every 12 hrs
+
+/*
+		//for demo only.
+		for (int i = 0; i <= 5; i++) {
+			System.out.println("Execution in Main Thread...." + i);
+			Thread.sleep(120000);
+			if (i == 5) {
+				System.out.println("Application Terminates");
+				System.exit(0);
+			}
+		} */
+
+	} catch ( Exception e)
+	{
+	}
+
+	}
+}
